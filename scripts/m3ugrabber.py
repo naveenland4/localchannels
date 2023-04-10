ddaa0c0aa1898dccd3fad94bfe79cc122a1e8405
naveenland4 <32125555+naveenland4@users.noreply.github.com>
2023-04-10 22:55:08 +0530

    Update m3ugrabber.py

--- scripts/m3ugrabber.py
+++ scripts/m3ugrabber.py
@@ -18,18 +18,6 @@ for channel in channel_data:
 
     streaming_url = channel['url']
     grab(url)
-
-m3u8_channel_list = open('../m3u8_channel_list.json')
-m3u8_channel_data = json.load(m3u8_channel_list)
-for channel in m3u8_channel_data:
-    grp_title = channel['group-title']
-    channel_logo = channel['tvg-logo']
-    channel_id = channel['tvg-id']
-    channel_name = channel['channel_name']
-    print(
-        f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{channel_logo}" tvg-id="{channel_id}", {channel_name}')
-
-    url = channel['url']
     print(url)
 
 if 'temp.txt' in os.listdir():
