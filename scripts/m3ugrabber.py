3882d1d0658a3cc530cc454d69c27507c5b912b9
naveenland4 <32125555+naveenland4@users.noreply.github.com>
2023-04-10 17:50:17 +0530

    Update m3ugrabber.py

--- scripts/m3ugrabber.py
+++ scripts/m3ugrabber.py
@@ -4,30 +4,6 @@ import json
 import requests
 
 
-def grab(url):
-    response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print(
-                    'https://raw.githubusercontent.com/firofame/Malayalam-IPTV/main/assets/moose_na.m3u')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner: end]:
-            link = response[end-tuner: end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
-
-
 print('#EXTM3U')
 
 channel_list = open('../channel_list.json')
