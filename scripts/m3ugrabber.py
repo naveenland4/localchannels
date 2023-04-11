4091ab24ea31a8c10c8f4f7c790c4c12d0fefcc5
naveenland4 <32125555+naveenland4@users.noreply.github.com>
2023-04-11 17:09:27 +0530

    Update m3ugrabber.py

--- scripts/m3ugrabber.py
+++ scripts/m3ugrabber.py
@@ -1,25 +1,36 @@
-#! /usr/bin/python3
-import os
-import json
-import requests
+name: Convert JSON to M3U
 
+on:
+  push:
+    paths:
+      - '*.json' # Trigger only on JSON file changes
 
-print('#EXTM3U')
+jobs:
+  convert:
+    runs-on: ubuntu-latest
 
-channel_list = open('../channel_list.json')
-channel_data = json.load(channel_list)
-for channel in channel_list:
-    grp_title = channel['group-title']
-    channel_logo = channel['tvg-logo']
-    channel_id = channel['tvg-id']
-    channel_name = channel['channel_name']
-    print(
-        f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{channel_logo}" tvg-id="{channel_id}", {channel_name}')
+    steps:
+    - name: Checkout Repository
+      uses: actions/checkout@v2
 
-    streaming_url = channel['url']
-    grab(url)
-    print(url)
+    - name: Install Python
+      uses: actions/setup-python@v2
+      with:
+        python-version: '3.9'
 
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+    - name: Install Dependencies
+      run: |
+        python -m pip install --upgrade pip
+        pip install json2m3u
+
+    - name: Convert JSON to M3U
+      run: |
+        python convert.py input.json output.m3u
+
+    - name: Commit Changes
+      run: |
+        git config --global user.name "${{ github.actor }}"
+        git config --global user.email "${{ github.actor }}@users.noreply.github.com"
+        git add output.m3u
+        git commit -m "Convert JSON to M3U"
+        git push
